ec9886d38e31a37cb311844d6f4898b27ea40569
57494324+enuberia-43@users.noreply.github.com <enuberia-43@users.noreply.github.com>
2022-12-30 14:12:07 +0900

    コミット＆プッシュテスト

--- SensorSystem/Program.cs
+++ SensorSystem/Program.cs
@@ -14,6 +14,7 @@ namespace SensorSystem
         [STAThread]
         static void Main()
         {
+            //Gitテストコメント
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
